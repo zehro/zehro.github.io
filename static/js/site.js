8ea2e5f8ef452a7316f2e6b4b9d359841dbcef32
zehro <wisdom0music@gmail.com>
2017-10-07 23:40:44 -0400

    Testing coding section on live pages

--- static/js/site.js
+++ static/js/site.js
@@ -1,14 +1,24 @@
 $(initialize);
 
 var animationSpeed = 500;
+var visited = false;
 
 function initialize() {
+    loadTemplates();
+    setupNavbar();
+
+    $('.vgdev_carousel').slick();
+}
+
+function loadTemplates() {
     $('#intro').load('templates/main.intro.html');
     $('#coding').load('templates/main.coding.html');
     $('#design').load('templates/main.design.html');
     $('#about').load('templates/main.about.html');
     $('#contact').load('templates/main.contact.html');
+}
 
+function setupNavbar() {
     $('.logo').click(function(e) {
         e.preventDefault();
         if ($('#intro').hasClass('pt-moveToRightFade'))
@@ -49,26 +59,47 @@ function initialize() {
 function visitPage(page, link) {
     if ($(page).hasClass('pt-moveToLeftFade'))
     {
+        // Move page in
         $(page).addClass('pt-moveFromLeftFade');
 
+        // Move other pages out
         $('#intro').addClass('pt-moveToRightFade');
         $('#coding, #design, #about, #contact').not(page).addClass('pt-moveToLeftFade');
 
+        // Set the active navbar link
         $(link).addClass('active');
         $('.coding, .design, .about, .contact').not(link).removeClass('active');
 
+        // Remove animation classes after animation finishes
         setTimeout(function () {
             $(page).removeClass();
         }, animationSpeed);
     }
 
-    if (page == '#coding')
+    if (page == '#coding' && !visited)
     {
-        setTimeout(function () {
-            $('.fragment').addClass('start');
-        }, animationSpeed);
-        setTimeout(function () {
-            $('.fragment').removeClass('start');
-        }, 8000);
+        playCodingTitle();
     }
+}
+
+function playCodingTitle() {
+    // Toggle the visited bool
+    visited = true;
+
+    // Play the animation for first fragment
+    setTimeout(function () {
+        $('#fragment1').addClass('start');
+    }, animationSpeed);
+
+    // Play the animation for second fragment
+    setTimeout(function () {
+        $('#fragment1').removeClass('start');
+        $('#fragment2').addClass('start');
+    }, animationSpeed + 2000);
+
+    // Play the animation for final fragment
+    setTimeout(function () {
+        $('#fragment2').removeClass('start');
+        $('#fragment3').addClass('start');
+    }, animationSpeed + 4000);
 }
